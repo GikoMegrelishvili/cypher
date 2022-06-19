2ca827d4da8ff7711852a6f215486d5be3f53062
deliverymaan <deliverymaan@gmail.com>
2022-06-18 18:01:55 -0700

    register forms validation, confirm password validation and invalid error messages

--- src/app/admin/containers/register/register.component.ts
+++ src/app/admin/containers/register/register.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,38 +15,51 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  public form: FormGroup = this.returnInitializedForm();
+  public form!: FormGroup;
 
   constructor(private _fb: FormBuilder, private _store: AngularFirestore) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = this._fb.group(
+      {
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(20),
+          ],
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.pattern(
+              '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{7,}'
+            ),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validators: this.passwordMatchingValidator }
+    );
+  }
 
   public onAddUser(): void {
     if (this.form.invalid) return;
-    console.log('Request');
     this._store.collection('admins').add(this.form.value);
   }
 
-  private returnInitializedForm(): FormGroup {
-    return this._fb.group({
-      username: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(20),
-        ],
-      ],
-      email: ['', [Validators.required, Validators.email]],
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(6),
-          // Validators.pattern('^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$'),
-        ],
-      ],
-      confirmPassword: ['', [Validators.required]],
-    });
-  }
+  passwordMatchingValidator: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    return password?.value === confirmPassword?.value
+      ? null
+      : { notMatched: true };
+  };
 }
