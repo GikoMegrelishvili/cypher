891c53bfdf4fb221b2a07ae8497e0fe3a90b68eb
deliverymaan <deliverymaan@gmail.com>
2022-06-22 14:01:08 -0700

    header and nav bar logo

--- src/app/public/overview/overview.module.ts
+++ src/app/public/overview/overview.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CoreModule } from 'src/app/core/core.module';
 import { OverviewPageComponent } from './overview-page/overview-page.component';
 import { OverviewPageRoutingModule } from './overview-routing.module';
 
 @NgModule({
   declarations: [OverviewPageComponent],
-  imports: [CommonModule, OverviewPageRoutingModule],
+  imports: [CommonModule, OverviewPageRoutingModule, CoreModule],
 })
 export class OverviewModule {}
